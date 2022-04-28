29d01e8347bac9c7d67087c6124df980d7fbb035
Andy Stewart <lazycat.manatee@gmail.com>
2022-04-28 20:17:26 +0800

    First commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# What is recursive-search-references.el ?
+Sometimes I want to know if an Elisp extension I installed is used? Delete it if it is not used
+
+So we open extension file, find every function name, then use ripgrep search it in my Emacs config directory, over and over again, if there are many functions define in extension, this process will be very, very painful.
+
+recursive-search-references help you do those process automatically:
+1. Use tree-sitter list all functions in current extension
+2. Use ripgrep search every functions in you special directory (usually is your emacs configuration directory), but except the directory that contain current extension
+3. It will notify you "remove current extension safely" if no any reference found in directory
+
+## Installation
+1. Install tree-sitter first: https://emacs-tree-sitter.github.io/installation/
+2. Install [ripgrep](https://github.com/BurntSushi/ripgrep)
+3. Clone or download this repository (path of the folder is the `<path-to-recursive-search-references>` used below).
+
+In your `~/.emacs`, add the following two lines:
+```Elisp
+(add-to-list 'load-path "<path-to-recursive-search-references>") ; add recursive-search-references to your load-path
+(require 'recursive-search-references)
+```
+
+## Usage
+* recursive-search-references : find function references in directory
---
+++ recursive-search-references.el
@@ -0,0 +1,134 @@
+;;; recursive-search-references.el --- Recursive search references
+
+;; Filename: recursive-search-references.el
+;; Description: Recursive search references
+;; Author: Andy Stewart <lazycat.manatee@gmail.com>
+;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
+;; Copyright (C) 2021, Andy Stewart, all rights reserved.
+;; Created: 2021-11-27 10:50:57
+;; Version: 0.1
+;; Last-Updated: 2021-11-27 10:50:57
+;;           By: Andy Stewart
+;; URL: https://www.github.org/manateelazycat/recursive-search-references
+;; Keywords:
+;; Compatibility: GNU Emacs 29.0.50
+;;
+;; Features that might be required by this library:
+;;
+;; `tree-sitter'
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; Recursive search references
+;;
+
+;;; Installation:
+;;
+;; Put recursive-search-references.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'recursive-search-references)
+;;
+;; No need more.
+
+;;; Customize:
+;;
+;;
+;;
+;; All of the above can customize by:
+;;      M-x customize-group RET recursive-search-references RET
+;;
+
+;;; Change log:
+;;
+;; 2021/11/27
+;;      * First released.
+;;
+
+;;; Acknowledgements:
+;;
+;;
+;;
+
+;;; TODO
+;;
+;;
+;;
+
+;;; Require
+(require 'cl-lib)
+(require 'tree-sitter)
+
+;;; Code:
+
+(defvar recursive-search-references-search-dir nil)
+(defvar recursive-search-references-ignore-dir nil)
+
+(defun recursive-search-references-get-match-nodes (match-rule)
+  (ignore-errors
+    (let* ((query (tsc-make-query tree-sitter-language match-rule))
+           (root-node (tsc-root-node tree-sitter-tree))
+           (captures (mapcar #'cdr (tsc-query-captures query root-node #'tsc--buffer-substring-no-properties))))
+      captures)))
+
+(defun recursive-search-references-match-times-in-directory (search-string)
+  (let* ((search-command (format "rg --no-ignore -g '!node_modules' -g '!dist' -g '%s' %s %s --stats -q"
+                                 recursive-search-references-ignore-dir
+                                 search-string
+                                 recursive-search-references-search-dir))
+         (search-result (shell-command-to-string search-command)))
+    (string-to-number (nth 0 (split-string (nth 1 (split-string search-result "\n")))))))
+
+(defun recursive-search-references-function (match-times-func location)
+  (interactive)
+  (let* ((function-nodes (append
+                          (recursive-search-references-get-match-nodes "(function_definition name: (symbol) @x)")
+                          (recursive-search-references-get-match-nodes "(function_definition name: (identifier) @x)")
+                          (recursive-search-references-get-match-nodes "(method_declaration name: (identifier) @x)")
+                          (recursive-search-references-get-match-nodes "(function_declaration name: (identifier) @x)")
+                          ))
+         (function-names (mapcar #'tsc-node-text function-nodes))
+         (reference-functions (cl-remove-if #'(lambda (f) (<= (funcall match-times-func f) 1)) function-names)))
+    (message "*** %s" function-names)
+    (if (> (length reference-functions) 0)
+        (progn
+          (message "Found below reference functions in current %s." location)
+          (message "--------")
+          (mapcar #'(lambda (f) (message "%s" f)) reference-functions)
+          (message "--------")
+          (message "Found %s reference functions, switch to buffer `*Messages*' to review." (length reference-functions)))
+      (message "No references found in directory %s, you can remove current extension safely." location))))
+
+(defun recursive-search-references ()
+  (interactive)
+  (setq recursive-search-references-search-dir (expand-file-name (read-directory-name "Recursive search references at directory: ")))
+  (setq recursive-search-references-ignore-dir default-directory)
+  (recursive-search-references-function 'recursive-search-references-match-times-in-directory "directory"))
+
+(provide 'recursive-search-references)
+
+;;; recursive-search-references.el ends here
